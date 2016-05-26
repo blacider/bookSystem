9c69574d02c0d783c315f92f86a16b52598c7bca
niurenpeng <niurenpeng@niurenpeng.com>
2016-05-27 01:12:19 +0800

    banner  进行循环轮播，并在该停的时候暂停轮播

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,52 @@
 (function(window, undefined) {
     jQuery(document).ready(function($) {
         (function bannerInit() {
-            var container = $(".banner ul");
+            var container = $(".banner-container");
             var size = container.find('li').length;
-            container.width(size*169);
+            container.width(size*169*2);
+            var list1 = container.find("ul");
+            var list2 = list1.clone(true);
+            list1.after(list2);
             var current = 1;
             var moveTo = function(dest) {
-                dest = dest > size-7 ? size-6 : dest;
+                if (dest == 0) {
+                    container.css('left', -size*169);
+                    dest = 8;
+                }
                 container.animate({
                     left: -(dest-1)*169
-                });
-                current = dest;
-            }
-            $(".prev").click(function(event) {
-                if (current > 1) {
-                    moveTo(current-1);
+                },function() {
+                    current = dest;
+                    if (dest > size) {
+                        container.css('left', 0);
+                        current = 1;
+                    }
                     console.log(current);
-                }
+                });
+            }
+            var intervalId = setInterval(function() {
+                moveTo(current+1)
+            }, 2000);
+            $(".prev").click(function(event) {
+                moveTo(current-1);
+                clearInterval(intervalId);
+                intervalId = setInterval(function() {
+                    moveTo(current+1)
+                }, 2000);
             });
             $(".next").click(function(event) {
                 moveTo(current+1);
-                console.log(current);
+                clearInterval(intervalId);
+                intervalId = setInterval(function() {
+                    moveTo(current+1)
+                }, 2000);
+            });
+            container.mouseleave(function(event) {
+                intervalId = setInterval(function() {
+                    moveTo(current+1)
+                }, 2000);
+            }).mouseenter(function(event) {
+                clearInterval(intervalId);
             });
         })();
         (function bendEvent() {
